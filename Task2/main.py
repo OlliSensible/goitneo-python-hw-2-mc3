6bfaa6ab7a9d5c797d91b672a7cc711654950e74
OlliSensible <127432509+OlliSensible@users.noreply.github.com>
2023-10-25 17:21:35 +0200

    update

--- Task2/main.py
+++ Task2/main.py
@@ -1,16 +1,20 @@
-class Phone:
+class Field:
+    def __init__(self, value):
+        self.value = value
+
+class Phone(Field):
     def __init__(self, number):
         if not self.is_valid(number):
             raise ValueError("Invalid phone number")
-        self.number = number
+        super().__init__(number)
 
     @staticmethod
     def is_valid(number):
         return len(number) == 10 and number.isdigit()
 
-class Name:
+class Name(Field):
     def __init__(self, name):
-        self.name = name
+        super().__init__(name)
 
 class Record:
     def __init__(self, name, phone):
@@ -25,7 +29,7 @@ class Record:
 
     def remove_phone(self, phone):
         for p in self.phones:
-            if p.number == phone:
+            if p.value == phone:
                 self.phones.remove(p)
                 break
         else:
@@ -36,15 +40,15 @@ class Record:
             raise ValueError("Invalid new phone number")
 
         for p in self.phones:
-            if p.number == old_phone:
-                p.number = new_phone
+            if p.value == old_phone:
+                p.value = new_phone
                 break
         else:
             raise KeyError("Phone number not found")
 
     def find_phone(self, phone):
         for p in self.phones:
-            if p.number == phone:
+            if p.value == phone:
                 return p
         else:
             raise KeyError("Phone number not found")
@@ -75,15 +79,6 @@ def input_error(func):
     def wrapper(*args, **kwargs):
         try:
             return func(*args, **kwargs)
-        except (KeyError, ValueError, IndexError) as e:
-            return str(e)
-
-    return wrapper
-
-def input_error(func):
-    def wrapper(*args, **kwargs):
-        try:
-            return func(*args, **kwargs)
         except KeyError:
             return "Enter user name."
         except ValueError:
@@ -109,7 +104,7 @@ def handle_change(command, address_book):
     _, name, phone = command.split()
     name = name.lower()
     record = address_book.find(name)
-    record.edit_phone(record.phones[0].number, phone)
+    record.edit_phone(record.phones[0].value, phone)
     return "Contact changed."
 
 @input_error
@@ -117,7 +112,7 @@ def handle_phone(command, address_book):
     _, name = command.split()
     name = name.lower()
     record = address_book.find(name)
-    return f"Phone number for {name}: {record.phones[0].number}"
+    return f"Phone number for {name}: {record.phones[0].value}"
 
 @input_error
 def handle_delete(command, address_book):
@@ -130,7 +125,7 @@ def handle_delete(command, address_book):
 def handle_all(address_book):
     result = "All records:\n"
     for name, record in address_book.data.items():
-        result += f"{name}: {', '.join([p.number for p in record.phones])}\n"
+        result += f"{name}: {', '.join([p.value for p in record.phones])}\n"
     return result
 
 def main():
